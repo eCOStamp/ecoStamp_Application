3655447ca5def6c410a7a5217d92f5a6f81d9ad9
NachaiLim <khim1204@gmail.com>
2014-08-31 02:23:45 +0900

    collect veiw
    collect veiw

--- src/com/example/cicp_project/collect_stamp.java
+++ src/com/example/cicp_project/collect_stamp.java
@@ -1,17 +1,31 @@
 package com.example.cicp_project;
 
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+
 
 public class collect_stamp extends Fragment{
 	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 		View android = inflater.inflate(R.layout.collect_stamp, container, false);
-		((TextView)android.findViewById(R.id.textView)).setText("Collect Your Stamp");
+		ImageView img;
+		img= (ImageView) android.findViewById(R.id.img);
+		img.setImageResource(R.drawable.collect);
+		
+		Button CollectButton,ReturnButton;
+		CollectButton=(Button)android.findViewById(R.id.CollectButton);
+		ReturnButton=(Button)android.findViewById(R.id.CancelButton);
+		CollectButton.setVisibility(View.GONE);
+		ReturnButton.setVisibility(View.GONE);
+		
 		return android;
 	}
 }
